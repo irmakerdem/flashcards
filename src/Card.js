3825f9cc30f20fda06ff0041238c757134474896
Irmak Erdem <irmakerdem9@gmail.com>
2022-05-09 20:06:46 -0600

    Complete card class

---
+++ src/Card.js
@@ -0,0 +1,9 @@
+class Card {
+    constructor(id, quest, ans, cor) {
+        this.id = id;
+        this.question = quest;
+        this.answers = ans;
+        this.correctAnswer = cor;
+    }
+    
+module.exports = Card;
