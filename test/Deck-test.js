b0b7ceb5623e7fa14aa4a51903a30cde3ba8e3d1
Irmak Erdem <irmakerdem9@gmail.com>
2022-05-11 19:51:32 -0600

    Change file name, edit Deck constructor, add round files

--- test/Deck-test.js
+++ test/Deck-test.js
@@ -19,7 +19,13 @@ describe('Deck', function() {
     }); 
   
     it('should hold an array of card objects', () => {
-        const deck = new Deck();
+        const card1 = new Card(2, "What is a comma-separated list of related values?", ["array", "object", "function"], "array");
+        const card2 = new Card(16, "What does the callback function for reduce() return?", ["the accumulator", "the current element", "the initializer"], "the accumulator");
+        const card3 = new Card(19, "What does the callback function for filter() return?", ["boolean", "an array of the same length", "a single value of any data type"], "boolean");
+        const card4 = new Card(24, "Object.assign() can take in an indefinite amount of arguments", ["true", "false"], "true");
+        const card5 = new Card(29, "map() takes in two optional arguments: the index of the current element, and the array that map was called upon", ["true", "false"], "true");
+
+        const deck = new Deck([card1, card2, card3, card4, card5]); 
 
         expect(deck.cards).to.be.an('array');
     }); 
@@ -31,10 +37,7 @@ describe('Deck', function() {
         const card4 = new Card(24, "Object.assign() can take in an indefinite amount of arguments", ["true", "false"], "true");
         const card5 = new Card(29, "map() takes in two optional arguments: the index of the current element, and the array that map was called upon", ["true", "false"], "true");
 
-        const deck = new Deck();
-
-        deck.cards.push(card1, card2, card3, card4, card5);
-        deck.countCards();
+        const deck = new Deck([card1, card2, card3, card4, card5]); 
 
         expect(deck.countCards()).to.equal(5);
       }); 
