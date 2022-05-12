4a08b0751af44b69a13c83cf9726a7aa2cd6a737
Irmak Erdem <irmakerdem9@gmail.com>
2022-05-12 17:04:12 -0600

    Remove comments and fix spacing

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -1,6 +1,5 @@
 const chai = require('chai');
 const expect = chai.expect;
-
 const Turn = require('../src/Turn');
 const Card = require('../src/Card');
 
@@ -47,7 +46,6 @@ describe('Turn', () => {
       });
 
       it('should check if the user guessed correct answer', function() {
-
         turn1.evaluateGuess();
 
         expect(turn1.evaluateGuess()).to.equal(false);
@@ -69,78 +67,3 @@ describe('Turn', () => {
         expect(turn2.giveFeedback()).to.equal('correct!');
       });
 });
-
-// describe('Turn', function() {
-//   it('should be a function', () => {
-//       const turn = new Turn();
-//       expect(Turn).to.be.a('function');
-//   });
-
-//   it('should be an instance of Turn', () => {
-//       const turn = new Turn();
-//       expect(turn).to.be.an.instanceof(Turn);
-//   }); 
-
-//   it('should store the user\'s guess to question', () => {
-//       const turn = new Turn('Object.keys()');
-//       expect(turn.guess).to.be.a('string');
-//       expect(turn.guess).to.equal('Object.keys()');
-//   }); 
-
-//   it('should store any user\'s guess to question', () => {
-//       const turn = new Turn("filter()");
-//       expect(turn.guess).to.equal("filter()");
-//   }); 
-
-//   it('should store a current card as an object', () => {
-//       const card = new Card(23, "Which prototype method can make a copy of a pre-existing object or merge two or more objects together?", ["Object.assign()", "Object.keys()", "filter()"], "Object.assign()");
-//       const turn = new Turn("filter()", card);
-//       expect(turn.card).to.equal(card);
-//   }); 
-
-//   it('should return the user\'s guess', function() {
-//       const card = new Card(23, "Which prototype method can make a copy of a pre-existing object or merge two or more objects together?", ["Object.assign()", "Object.keys()", "filter()"], "Object.assign()");
-//       const turn = new Turn("filter()", card);
-      
-//       turn.returnCard();
-     
-//       expect(turn.returnGuess()).to.equal(turn.guess);
-//     });
-
-//     it('should return the user\'s card', function() {
-//       const card = new Card(23, "Which prototype method can make a copy of a pre-existing object or merge two or more objects together?", ["Object.assign()", "Object.keys()", "filter()"], "Object.assign()");
-//       const turn = new Turn("filter()", card);
-      
-//       turn.returnCard();
-     
-//       expect(turn.returnCard()).to.equal(turn.card);
-//     });
-
-//     it('should check if the user guessed correct answer', function() {
-//       const card = new Card(23, "Which prototype method can make a copy of a pre-existing object or merge two or more objects together?", ["Object.assign()", "Object.keys()", "filter()"], "Object.assign()");
-//       const turn1 = new Turn("filter()", card);
-//       const turn2 = new Turn("Object.assign()", card);
-
-//       turn1.evaluateGuess();
-
-//       expect(turn1.evaluateGuess()).to.equal(false);
-  
-//       turn2.evaluateGuess();
-//       expect(turn2.evaluateGuess()).to.equal(true);
-//     });
-
-//     it('should give feedback about the user\'s guess', function() {
-//       const card = new Card(23, "Which prototype method can make a copy of a pre-existing object or merge two or more objects together?", ["Object.assign()", "Object.keys()", "filter()"], "Object.assign()");
-//       const turn1 = new Turn("filter()", card);
-//       const turn2 = new Turn("Object.assign()", card);
-
-//       turn1.evaluateGuess();
-//       turn1.giveFeedback();
-
-//       expect(turn1.giveFeedback()).to.equal('incorrect!');
-  
-//       turn2.evaluateGuess();
-//       turn2.giveFeedback();
-//       expect(turn2.giveFeedback()).to.equal('correct!');
-//     });
-// });
