e0caac3ce7cc097a2e6b598cdba8c56c41edd7ce
Irmak Erdem <irmakerdem9@gmail.com>
2022-05-11 20:31:33 -0600

    Add turn incrementer

--- src/Round.js
+++ src/Round.js
@@ -1,9 +1,20 @@
 class Round {
     constructor(deck) {
         this.deck = deck.cards;
+        this.turns = 0;
     }
     returnCurrentCard() {
         return this.deck[0];
+    }
+    takeTurn() {
+        //updates turns count
+        this.turns++;
+        //evaluates guesses
+
+        //gives feedback
+
+        //stores ids of incorrect guesses
+
     }
 }
 
--- test/Round-test.js
+++ test/Round-test.js
@@ -8,6 +8,7 @@ const Round = require('../src/Round');
 
 describe('Round', function() {
     it('should be a function', () => {
+
         expect(Round).to.be.a('function');
     });
 
@@ -19,10 +20,11 @@ describe('Round', function() {
         const card5 = new Card(29, "map() takes in two optional arguments: the index of the current element, and the array that map was called upon", ["true", "false"], "true");
         const deck = new Deck([card1, card2, card3, card4, card5]);
         const round = new Round(deck);
+
         expect(round).to.be.an.instanceof(Round);
     }); 
 
-    it('should have a current card', () => {
+    it('should return a current card', () => {
         const card1 = new Card(2, "What is a comma-separated list of related values?", ["array", "object", "function"], "array");
         const card2 = new Card(16, "What does the callback function for reduce() return?", ["the accumulator", "the current element", "the initializer"], "the accumulator");
         const card3 = new Card(19, "What does the callback function for filter() return?", ["boolean", "an array of the same length", "a single value of any data type"], "boolean");
@@ -35,4 +37,31 @@ describe('Round', function() {
         // expect(round.returnCurrentCard()).to.equal(round.deck[0]);
         expect(round.returnCurrentCard()).to.equal(card1);
     });
+
+    it('should have a turn counter', () => {
+        const card1 = new Card(2, "What is a comma-separated list of related values?", ["array", "object", "function"], "array");
+        const card2 = new Card(16, "What does the callback function for reduce() return?", ["the accumulator", "the current element", "the initializer"], "the accumulator");
+        const card3 = new Card(19, "What does the callback function for filter() return?", ["boolean", "an array of the same length", "a single value of any data type"], "boolean");
+        const card4 = new Card(24, "Object.assign() can take in an indefinite amount of arguments", ["true", "false"], "true");
+        const card5 = new Card(29, "map() takes in two optional arguments: the index of the current element, and the array that map was called upon", ["true", "false"], "true");
+        const deck = new Deck([card1, card2, card3, card4, card5]);
+        const round = new Round(deck);
+        
+        expect(round.turns).to.equal(0);
+    });
+
+    it('should be able to increment number of turns by 1', () => {
+        const card1 = new Card(2, "What is a comma-separated list of related values?", ["array", "object", "function"], "array");
+        const card2 = new Card(16, "What does the callback function for reduce() return?", ["the accumulator", "the current element", "the initializer"], "the accumulator");
+        const card3 = new Card(19, "What does the callback function for filter() return?", ["boolean", "an array of the same length", "a single value of any data type"], "boolean");
+        const card4 = new Card(24, "Object.assign() can take in an indefinite amount of arguments", ["true", "false"], "true");
+        const card5 = new Card(29, "map() takes in two optional arguments: the index of the current element, and the array that map was called upon", ["true", "false"], "true");
+        const deck = new Deck([card1, card2, card3, card4, card5]);
+        const round = new Round(deck);
+        
+        round.takeTurn();
+
+        expect(round.turns).to.equal(1);
+    });
+
 });
